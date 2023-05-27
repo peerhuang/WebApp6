1903e0146efc6f5da03a0b2a2760682591cddc3f
Huang Jie <peer_huang@msn.com>
2023-05-27 15:09:43 +0800

    ddd

---
+++ WebApp6/Grpc/ProtobufHelper.cs
@@ -0,0 +1,21 @@
+﻿using Google.Protobuf;
+using ProtoBuf;
+
+namespace WebApp6.Grpc
+{
+    public static class ProtobufHelper
+    {
+        public static ByteString Serialize<T>(T value)
+        {
+            using var mem = new MemoryStream();
+            Serializer.Serialize(mem, value);
+            return ByteString.FromStream(mem);
+        }
+
+        public static T Deserialize<T>(ByteString byteString)
+        {
+            using var mem = new MemoryStream(byteString.ToArray());
+            return Serializer.Deserialize<T>(mem);
+        }
+    }
+}
