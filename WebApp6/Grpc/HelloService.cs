4b452fb08139d027829c0458646322c638cffd28
Huang Jie <peer_huang@msn.com>
2023-05-27 13:52:51 +0800

    ddd

---
+++ WebApp6/Grpc/HelloService.cs
@@ -0,0 +1,25 @@
+﻿using Dapr.AppCallback.Autogen.Grpc.v1;
+using Dapr.Client.Autogen.Grpc.v1;
+using Grpc.Core;
+
+namespace WebApp6.Grpc
+{
+    public class HelloService : AppCallback.AppCallbackBase
+    {
+        public override async Task<InvokeResponse> OnInvoke(InvokeRequest request, ServerCallContext context)
+        {
+            switch (request.Method)
+            {
+                case "sayhi":
+                    var response = new InvokeResponse();
+                    //var input = request.Data.Unpack<HelloRequest>();
+                    //response.Data = Any.Pack(new HelloReply { Message = "ok" });
+                    return response;
+
+                default:
+
+                    return await base.OnInvoke(request, context);
+            }
+        }
+    }
+}
--- WebApp6/Program.cs
+++ WebApp6/Program.cs
@@ -1,3 +1,5 @@
+using WebApp6.Grpc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
+app.MapGrpcService<HelloService>();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
