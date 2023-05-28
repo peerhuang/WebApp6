db91224ad5e7ec26052674e255307cb3ea03078f
Huang Jie <peer_huang@msn.com>
2023-05-28 09:47:05 +0800

    dddd

--- WebApp6/Controllers/TestController.cs
+++ WebApp6/Controllers/TestController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 using System.Text.Json.Nodes;
 
 namespace WebApp6.Controllers
@@ -7,16 +8,20 @@ namespace WebApp6.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
+        private readonly IDiagnosticContext _diagnosticContext;
         private readonly ILogger<TestController> _logger;
 
-        public TestController(ILogger<TestController> logger)
+        public TestController(ILogger<TestController> logger, IDiagnosticContext diagnosticContext)
         {
             _logger = logger;
+            _diagnosticContext = diagnosticContext;
         }
 
         [HttpPost]
         public void Post(JsonNode node)
         {
+            _logger.LogDebug("111111111111111111111111111111111111111111111111111111111111111");
+            _diagnosticContext.Set("11232", 11111);
             _logger.LogInformation(node.ToJsonString());
         }
     }
