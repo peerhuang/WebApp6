e378427796d1ed267fa4c3d32ff27db426862c7c
Huang Jie <peer_huang@msn.com>
2023-05-28 09:56:59 +0800

    ddd

--- WebApp6/Controllers/TestController.cs
+++ WebApp6/Controllers/TestController.cs
@@ -8,20 +8,16 @@ namespace WebApp6.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
-        private readonly IDiagnosticContext _diagnosticContext;
         private readonly ILogger<TestController> _logger;
 
         public TestController(ILogger<TestController> logger, IDiagnosticContext diagnosticContext)
         {
             _logger = logger;
-            _diagnosticContext = diagnosticContext;
         }
 
         [HttpPost]
         public void Post(JsonNode node)
         {
-            _logger.LogDebug("111111111111111111111111111111111111111111111111111111111111111");
-            _diagnosticContext.Set("11232", 11111);
             _logger.LogInformation(node.ToJsonString());
         }
     }
